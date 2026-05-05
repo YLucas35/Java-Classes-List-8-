4e2e3400e0f2b581a95dc54542f8a3748ea80c4c
YLucas35 <0001244748@senaimgaluno.com.br>
2026-05-05 15:08:49 -0300

    Create _atividade_19.java

---
+++ _atividade_19.java
@@ -0,0 +1,11 @@
+package Java_Lista_8_Para;
+
+public class _atividade_19 {
+    public static void main(String[] args) {
+        for (int i = 10; i >= 0; i--) {
+            System.out.println(i);
+        }
+
+        System.out.println("Lançamento autorizado!");
+    }
+}
